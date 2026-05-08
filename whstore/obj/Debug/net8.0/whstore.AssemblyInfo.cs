0711cff15891e07a3c38af62ba919f69e287bb21
whidealionofficials-dotcom <whidealionofficials@gmail.com>
2026-05-08 11:06:31 +0600

    final: clean code with dark dashboard

--- whstore/obj/Debug/net8.0/whstore.AssemblyInfo.cs
+++ whstore/obj/Debug/net8.0/whstore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("whstore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c8f34cd0a0e666cd6750e25e5d906af8dad99e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+857314898171c4d57f269fcb8553c2b5b3871986")]
 [assembly: System.Reflection.AssemblyProductAttribute("whstore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("whstore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
